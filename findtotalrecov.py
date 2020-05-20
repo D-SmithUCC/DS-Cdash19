d01ad2b1374f5417f23ffae9d5d96e1c51274a7b
Y9Dsmith <58474567+Y9Dsmith@users.noreply.github.com>
2020-05-20 16:26:32 -0400

    Add files via upload

---
+++ findtotalrecov.py
@@ -0,0 +1,21 @@
+fi = open("05-19-2020.csv","r")
+fi.readline() # skip over first title line
+datarows = fi.readlines()
+fi.close()
+
+fo = open("totalrecov.txt","w")
+
+total= 0
+
+for line in datarows:
+    templist = line.split(",")
+    a= templist[9]
+    a= float(a)
+    a= int(a)
+    total= total +(a)
+    a= 0
+       
+fo.write (str(total))
+ 
+     
+fo.close()
